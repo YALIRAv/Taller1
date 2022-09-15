c4ab697107110e2db9c1957c2333a900dae53213
YALIRAv <yalirav2@hotmail.com>
2022-09-14 19:18:05 -0500

    first commit

---
+++ src/Taller1.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class taller1 {
+    
+}
+     public static void main(String[] args) {
+        int edad;
+        String nombre;
+     
+        Scanner entrada = new scanner (system.in);
+             
+    
+}
+      
